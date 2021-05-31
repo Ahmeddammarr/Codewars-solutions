0754f918b020179970b5f17b3fb6a62972ac6db7
Ammarr <ahmedmahmoud9597@gmail.com>
2021-05-31 16:33:15 +0200

    SpinWords solution

---
+++ spinWords.ts
@@ -0,0 +1,25 @@
+/**
+ 
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (like the name of this kata).
+
+Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
+Examples:
+
+spinWords("Hey fellow warriors") => "Hey wollef sroirraw" 
+spinWords("This is a test") => "This is a test" 
+spinWords("This is another test") => "This is rehtona test"
+
+ */
+
+export class Kata {
+  static spinWords(words: string) {
+    const WordsArray = words.split(" ");
+    WordsArray.map(
+      (item, index) =>
+        item.length >= 5 &&
+        WordsArray.splice(index, 1, item.split("").reverse().join(""))
+    );
+    return WordsArray.join(" ");
+  }
+}
